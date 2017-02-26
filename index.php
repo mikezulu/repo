0f9cc54b70daad341a114888948eba196333ece6
mikezulu <maznizar.exabytes@gmail.com>
2017-02-27 00:31:58 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo "<div style='text-align:center'><h1>HELLO EXABYTES</h1></div>";
-//$image ='http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg';
-//$imageData = base64_encode(file_get_contents($image));
-//echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
-<div style="float: right; clear: left;"><IMG src="http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg"></div>
+$image ='http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg';
+$imageData = base64_encode(file_get_contents($image));
+echo <div style="float: right; clear: left;">'<img src="data:image/jpeg;base64,'.$imageData.'">'</div>;
+//<div style="float: right; clear: left;"><IMG src="http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg"></div>
 ?>
