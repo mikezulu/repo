776a7fb0b2506243fc2fa385c74875853d788121
mikezulu <maznizar.exabytes@gmail.com>
2017-02-27 00:27:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<div style='text-align:center'><h1>HELLO EXABYTES</h1></div>";
-$image = <div align=center>'http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg'</div>;
+$image ='http://www.exabytes.asia/download/logo/ExabytesGroup600240bw.jpg';
 $imageData = base64_encode(file_get_contents($image));
 echo '<img src="data:image/jpeg;base64,'.$imageData.'">';
 ?>
